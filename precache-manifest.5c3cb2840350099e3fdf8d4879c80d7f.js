619530d87d116d24a65a75eec9a572decfb2ed45
romain herry <romainherry@MacBook-Pro.local>
2020-10-16 13:44:48 -0700

    Updates

---
+++ precache-manifest.5c3cb2840350099e3fdf8d4879c80d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde1876d621e02504c02b7ece15c622d",
+    "url": "/unit-4-assessment/index.html"
+  },
+  {
+    "revision": "25d858f2f9eae2b39f90",
+    "url": "/unit-4-assessment/static/css/main.a21c980e.chunk.css"
+  },
+  {
+    "revision": "19ea1771b88beb8b583c",
+    "url": "/unit-4-assessment/static/js/2.bf5bcacf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/unit-4-assessment/static/js/2.bf5bcacf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d858f2f9eae2b39f90",
+    "url": "/unit-4-assessment/static/js/main.e0a24daa.chunk.js"
+  },
+  {
+    "revision": "e118b053bc85ce527bb1",
+    "url": "/unit-4-assessment/static/js/runtime-main.5ab301f9.js"
+  }
+]);
